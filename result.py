5981d943e180427705be13aba311b5efc859b874
PriyadharshiniVenkateshan <135581465+PriyadharshiniVenkateshan@users.noreply.github.com>
2023-06-06 23:28:30 +0530

    Create result.py

---
+++ result.py
@@ -0,0 +1,19 @@
+result :
+
+print("Count of adult:", count_of_adult)
+print("Count of child:", count_of_child)
+print("Total number of persons:", total_number_of_person)
+print("Place:", place)
+print("The amount per head adult is: {:.2f}".format(amount_per_head_adult))
+print("The amount per head child is: {:.2f}".format(amount_per_head_child))
+print("Total amount of tickets: {:.2f}".format(total_amount_of_tickets))
+
+
+
+Count of adult: 9
+Count of child: 2
+Total number of persons: 11
+Place: Kashmir
+The amount per head adult is: 5000.00
+The amount per head child is: 2500.00
+Total amount of tickets: 50000.00
